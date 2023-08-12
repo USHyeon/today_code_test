4f06471f10281b30085c95c5c6caef7125d0879d
Markl <60209934+USHyeon@users.noreply.github.com>
2023-08-12 16:50:36 +0900

    Day9: 문자열 뒤에서 부터 n글자

--- TodayCodeTest/src/main/kotlin/Day09.kt
+++ TodayCodeTest/src/main/kotlin/Day09.kt
@@ -9,6 +9,11 @@ fun main(args: Array<String>) {
             arrayOf(intArrayOf(0, 4), intArrayOf(1, 2), intArrayOf(3, 5), intArrayOf(7, 7))
         )
     )
+
+    println("Solution 03")
+    println(solution03("ProgrammerS123", 11))
+    println(solution03("He110W0r1d", 5))
+
 }
 
 /**
@@ -35,4 +40,12 @@ fun solution02(myStrings: Array<String>, parts: Array<IntArray>): String {
     return parts.mapIndexed { index, (s, e) ->
         myStrings[index].substring(s, e + 1)
     }.joinToString("")
+}
+
+/**
+ * 문제 설명
+ * 문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+ */
+fun solution03(my_string: String, n: Int): String {
+    return my_string.substring(my_string.length - n)
 }
