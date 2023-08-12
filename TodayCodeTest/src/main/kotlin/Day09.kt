2021320585d2d525afdd0aba75eb59c8a554db68
Markl <60209934+USHyeon@users.noreply.github.com>
2023-08-12 16:13:11 +0900

    Day9: 부분 문자열 이어 붙여 문자열 만들기

--- TodayCodeTest/src/main/kotlin/Day09.kt
+++ TodayCodeTest/src/main/kotlin/Day09.kt
@@ -2,6 +2,13 @@ fun main(args: Array<String>) {
     println("Solution 01")
     println(solution01(arrayOf("0123456789", "9876543210", "9999999999999"), 50000, 5, 5).toList())
 
+    println("Solution 02")
+    println(
+        solution02(
+            arrayOf("progressive", "hamburger", "hammer", "ahocorasick"),
+            arrayOf(intArrayOf(0, 4), intArrayOf(1, 2), intArrayOf(3, 5), intArrayOf(7, 7))
+        )
+    )
 }
 
 /**
@@ -16,4 +23,16 @@ fun solution01(intStrs: Array<String>, k: Int, s: Int, l: Int): IntArray {
     }.filter {
         it > k
     }.toIntArray()
+}
+
+/**
+ * 문제 설명
+ * 길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어집니다. parts[i]는 [s, e] 형태로,
+ * my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미합니다. 각 my_strings의 원소의 parts에 해당하는 부분 문자열을
+ * 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
+ */
+fun solution02(myStrings: Array<String>, parts: Array<IntArray>): String {
+    return parts.mapIndexed { index, (s, e) ->
+        myStrings[index].substring(s, e + 1)
+    }.joinToString("")
 }
