94a961e08e452a7d0017fd7d2e424ac98559c6d0
Markl <60209934+USHyeon@users.noreply.github.com>
2023-08-13 04:35:38 +0900

    Day9: 접미사인지 확인하기

--- TodayCodeTest/src/main/kotlin/Day09.kt
+++ TodayCodeTest/src/main/kotlin/Day09.kt
@@ -18,6 +18,12 @@ fun main(args: Array<String>) {
     println(solution04("banana"))
     println(solution04("programmers"))
 
+    println("solution 05")
+    println(solution05("banana", "ana"))
+    println(solution05("banana", "nan"))
+    println(solution05("banana", "wxyz"))
+    println(solution05("banana", "abanana"))
+
 }
 
 /**
@@ -65,4 +71,16 @@ fun solution04(my_string: String): Array<String> {
     return my_string.mapIndexedNotNull { index, str ->
         my_string.takeLast(index + 1)
     }.sorted().toTypedArray()
+}
+
+/**
+ * 문제 설명
+ * 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+ * 문자열 my_string과 is_suffix가 주어질 때, is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+ */
+fun solution05(my_string: String, is_suffix: String): Int {
+    val mySuffixList = my_string.mapIndexedNotNull { index, _ ->
+        my_string.takeLast(index + 1)
+    }
+    return if (mySuffixList.contains(is_suffix)) 1 else 0
 }
