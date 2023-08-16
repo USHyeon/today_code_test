74a5e55c65c2411f66d7be7a3fccc76f437958ac
Markl <60209934+USHyeon@users.noreply.github.com>
2023-08-17 04:03:06 +0900

    Day10: 세로 읽기

--- TodayCodeTest/src/main/kotlin/Day10.kt
+++ TodayCodeTest/src/main/kotlin/Day10.kt
@@ -46,4 +46,24 @@ fun main(args: Array<String>) {
     println("solution03 : 문자열 뒤집기")
     println(solution03("Progra21Sremm3", 6, 12))
     println(solution03("Stanley1yelnatS", 4, 10))
+
+    println()
+
+    /**
+     * solution04 : 세로 읽기
+     * 문제 설명
+     * 문자열 my_string과 두 정수 m, c가 주어집니다.
+     * my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+     */
+    fun solution04(my_string: String, m: Int, c: Int): String {
+        my_string.chunked(m).also { row ->
+            return row.map { column ->
+                column[c - 1]
+            }.joinToString("")
+        }
+    }
+
+    println("solution04 : 세로 읽기")
+    println(solution04("ihrhbakrfpndopljhygc", 4, 2))
+    println(solution04("programmers", 1, 1))
 }
