482d622218e0c96053ac2ef5944fd7266966dec6
Markl <60209934+USHyeon@users.noreply.github.com>
2023-08-17 03:30:58 +0900

    Day10: 문자열 뒤집기

--- TodayCodeTest/src/main/kotlin/Day10.kt
+++ TodayCodeTest/src/main/kotlin/Day10.kt
@@ -29,4 +29,21 @@ fun main(args: Array<String>) {
     println(solution02("banana", "nan"))
     println(solution02("banana", "abcd"))
     println(solution02("banana", "bananan"))
+
+    println()
+
+    /**
+     * solution03 : 문자열 뒤집기
+     * 문제 설명
+     * 문자열 my_string과 정수 s, e가 매개변수로 주어질 때, my_string에서 인덱스 s부터 인덱스 e까지를 뒤집은 문자열을 return 하는 solution 함수를 작성해 주세요.
+     */
+    fun solution03(my_string: String, s: Int, e: Int): String {
+        my_string.substring(s, e + 1).also {
+            return my_string.replace(it, it.reversed())
+        }
+    }
+
+    println("solution03 : 문자열 뒤집기")
+    println(solution03("Progra21Sremm3", 6, 12))
+    println(solution03("Stanley1yelnatS", 4, 10))
 }
